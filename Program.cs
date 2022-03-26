2612097d7193ca02841ea3a2a398d54defd7b6ba
jen <nguy.jenni@gmail.com>
2022-03-25 22:41:44 -0500

    create payments class

--- Program.cs
+++ Program.cs
@@ -16,36 +16,8 @@ namespace LoginProgram {
                     
                     //taking orders!
                     Orders order = new Orders();
-                    Console.WriteLine("Is this correct? [Y] or [N]");
-                    string confirm = Console.ReadLine();
-
-                    //take payments
-                    if (!string.IsNullOrWhiteSpace(confirm) && confirm == "Y") {
-                    Console.WriteLine("How would you like to make your payment?");
-                    Console.WriteLine("Credit Card [A] r Debit Card [B]");
-                    string payment = Console.ReadLine();
-                    if (!string.IsNullOrWhiteSpace(payment) && 
-                        payment == "A") {
-                            Console.WriteLine("Enter your credit card number");
-                            string creditString = Console.ReadLine();
-                            Console.WriteLine("Please confirm. Is this correct?");
-                            string confirmCreditPayment = Console.ReadLine();
-                            if (!string.IsNullOrWhiteSpace(confirmCreditPayment) &&
-                                confirmCreditPayment == "Y") {
-                                    Console.WriteLine("Thank you for your payment. Your order is being processed.");
-                                }
-                    } else if (!string.IsNullOrWhiteSpace(payment) && 
-                        payment == "B") {
-                            Console.WriteLine("Enter your debit card number");
-                            string debitString = Console.ReadLine();
-                            Console.WriteLine("Please confirm. Is this correct?");
-                            string confirmDebitPayment = Console.ReadLine();
-                            if (!string.IsNullOrWhiteSpace(confirmDebitPayment) && 
-                                confirmDebitPayment == "Y") {
-                                    Console.WriteLine("Thank you for your payment. Your order is being processed.");
-                                }
-                        }
-                }
+                    //taking payments
+                    Payments payment = new Payments();
 
                 } else if ((string.IsNullOrWhiteSpace(password))) {
                     Console.WriteLine("Password incorrect");
--- SignUp.cs
+++ SignUp.cs
@@ -20,34 +20,8 @@ public class SignUp {
             //logged in!
             //taking orders!
             Orders order = new Orders();
-                Console.WriteLine("Is this correct? [Y] or [N]");
-                string confirm = Console.ReadLine();
-                if (!string.IsNullOrWhiteSpace(confirm) && confirm == "Y") {
-                    Console.WriteLine("How would you like to make your payment?");
-                    Console.WriteLine("Credit Card [A] r Debit Card [B]");
-                    string payment = Console.ReadLine();
-                    if (!string.IsNullOrWhiteSpace(payment) && 
-                        payment == "A") {
-                            Console.WriteLine("Enter your credit card number");
-                            string creditString = Console.ReadLine();
-                            Console.WriteLine("Please confirm. Is this correct?");
-                            string confirmCreditPayment = Console.ReadLine();
-                            if (!string.IsNullOrWhiteSpace(confirmCreditPayment) &&
-                                confirmCreditPayment == "Y") {
-                                    Console.WriteLine("Thank you for your payment. Your order is being processed.");
-                                }
-                    } else if (!string.IsNullOrWhiteSpace(payment) && 
-                        payment == "B") {
-                            Console.WriteLine("Enter your debit card number");
-                            string debitString = Console.ReadLine();
-                            Console.WriteLine("Please confirm. Is this correct?");
-                            string confirmDebitPayment = Console.ReadLine();
-                            if (!string.IsNullOrWhiteSpace(confirmDebitPayment) && 
-                                confirmDebitPayment == "Y") {
-                                    Console.WriteLine("Thank you for your payment. Your order is being processed.");
-                                }
-                        }
-                }
+            //taking payments
+            Payments payment = new Payments();
             }
         } else {
             Console.WriteLine("Sign Up!");

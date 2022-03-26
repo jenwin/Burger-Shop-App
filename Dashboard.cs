caf50a27594e8eb3082b0d89e4ab7a5a27f63d26
jen <nguy.jenni@gmail.com>
2022-03-25 22:01:58 -0500

    add payment logic

--- Dashboard.cs
+++ Dashboard.cs
@@ -1,21 +1,23 @@
 using System;
 
 public class Dashboard {
-    //data members
-    public string tomato;
-    public string lettuce;
-    public string cheese;
-    public string sauce;
-    public string bbq;
-    public string ketchup;
-    //create dashboard constructor
-    public Dashboard(string tomato, string cheese, string lettuce, string sauce, 
-    string bbq, string ketchup) {
-        this.tomato = tomato;
-        this.cheese = cheese;
-        this.lettuce = lettuce;
-        this.sauce = sauce;
-        this.bbq = bbq;
-        this.ketchup = ketchup;
-    }
+
+    // //data members
+    // public string tomato;
+    // public string lettuce;
+    // public string cheese;
+    // public string sauce;
+    // public string bbq;
+    // public string ketchup;
+    // //create dashboard constructor
+    // public Dashboard(string tomato, string cheese, string lettuce, string sauce, 
+    // string bbq, string ketchup) {
+    //     this.tomato = tomato;
+    //     this.cheese = cheese;
+    //     this.lettuce = lettuce;
+    //     this.sauce = sauce;
+    //     this.bbq = bbq;
+    //     this.ketchup = ketchup;
+    // }
+    
 }
--- Program.cs
+++ Program.cs
@@ -42,6 +42,35 @@ namespace LoginProgram {
                         "Ketchup Sauce: " + ketchup);
                     Console.WriteLine("Is this correct? [Y] or [N]");
                     string confirm = Console.ReadLine();
+
+                    //take payments
+                    if (!string.IsNullOrWhiteSpace(confirm) && confirm == "Y") {
+                    Console.WriteLine("How would you like to make your payment?");
+                    Console.WriteLine("Credit Card [A] r Debit Card [B]");
+                    string payment = Console.ReadLine();
+                    if (!string.IsNullOrWhiteSpace(payment) && 
+                        payment == "A") {
+                            Console.WriteLine("Enter your credit card number");
+                            string creditString = Console.ReadLine();
+                            Console.WriteLine("Please confirm. Is this correct?");
+                            string confirmCreditPayment = Console.ReadLine();
+                            if (!string.IsNullOrWhiteSpace(confirmCreditPayment) &&
+                                confirmCreditPayment == "Y") {
+                                    Console.WriteLine("Thank you for your payment. Your order is being processed.");
+                                }
+                    } else if (!string.IsNullOrWhiteSpace(payment) && 
+                        payment == "B") {
+                            Console.WriteLine("Enter your debit card number");
+                            string debitString = Console.ReadLine();
+                            Console.WriteLine("Please confirm. Is this correct?");
+                            string confirmDebitPayment = Console.ReadLine();
+                            if (!string.IsNullOrWhiteSpace(confirmDebitPayment) && 
+                                confirmDebitPayment == "Y") {
+                                    Console.WriteLine("Thank you for your payment. Your order is being processed.");
+                                }
+                        }
+                }
+
                 }
 
                 } else if ((string.IsNullOrWhiteSpace(password))) {

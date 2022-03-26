a0e070413c00cc73da73cb15e332e057105426fc
jen <nguy.jenni@gmail.com>
2022-03-25 21:26:29 -0500

    add order logic

--- Program.cs
+++ Program.cs
@@ -13,7 +13,37 @@ namespace LoginProgram {
                 string password = Console.ReadLine();
                 if (!string.IsNullOrWhiteSpace(password)) {
                     Console.WriteLine("Logged in!");
-                    Dashboard dashboardObj = new Dashboard();
+                    
+                    Console.WriteLine("Can I take your order? [Y] or [N]");
+                    string takeOrder = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(takeOrder)) {
+                    Console.WriteLine("Would you like tomatoes? [Y] or [N]");
+                    string tomato = Console.ReadLine();
+                    Console.WriteLine("Would you like lettuce? [Y] or [N]");
+                    string lettuce = Console.ReadLine();
+                    Console.WriteLine("Would you like cheese? [Y] or [N]");
+                    string cheese = Console.ReadLine();
+                    Console.WriteLine("Would you like tomatoes? [Y] or [N]");
+                    string tomatoes = Console.ReadLine();
+                    Console.WriteLine("Would you like sauce? [Y] or [N]");
+                    string sauce = Console.ReadLine();
+                    Console.WriteLine("Would you like bbq? [Y] or [N]");
+                    string bbq = Console.ReadLine();
+                    Console.WriteLine("Would you like ketchup? [Y] or [N]");
+                    string ketchup = Console.ReadLine();
+                    Console.WriteLine("Please confirm your order.");
+                    // Dashboard order = new Dashboard("Y", "Y", "Y", "Y", "Y", "Y");
+                    Console.WriteLine(
+                        "Tomato: " + tomato + " " + 
+                        "Lettuce: " + lettuce + " " + 
+                        "Cheese: " + cheese + " " +
+                        "Sauce: " + sauce + " " + 
+                        "Bbq Sauce: " + bbq + " " + 
+                        "Ketchup Sauce: " + ketchup);
+                    Console.WriteLine("Is this correct? [Y] or [N]");
+                    string confirm = Console.ReadLine();
+                }
+
                 } else if ((string.IsNullOrWhiteSpace(password))) {
                     Console.WriteLine("Password incorrect");
                 }
--- SignUp.cs
+++ SignUp.cs
@@ -20,7 +20,38 @@ public class SignUp {
             Console.WriteLine("Thank you for signing up!");
 
             //logged in!
-            Dashboard dashboardObj = new Dashboard();
+            Console.WriteLine("Can I take your order? [Y] or [N]");
+            string takeOrder = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(takeOrder)) {
+                Console.WriteLine("Would you like tomatoes? [Y] or [N]");
+                string tomato = Console.ReadLine();
+                Console.WriteLine("Would you like lettuce? [Y] or [N]");
+                string lettuce = Console.ReadLine();
+                Console.WriteLine("Would you like cheese? [Y] or [N]");
+                string cheese = Console.ReadLine();
+                Console.WriteLine("Would you like tomatoes? [Y] or [N]");
+                string tomatoes = Console.ReadLine();
+                Console.WriteLine("Would you like sauce? [Y] or [N]");
+                string sauce = Console.ReadLine();
+                Console.WriteLine("Would you like bbq? [Y] or [N]");
+                string bbq = Console.ReadLine();
+                Console.WriteLine("Would you like ketchup? [Y] or [N]");
+                string ketchup = Console.ReadLine();
+                Console.WriteLine("Please confirm your order.");
+                // Dashboard order = new Dashboard("Y", "Y", "Y", "Y", "Y", "Y");
+                Console.WriteLine(
+                    "Tomato: " + tomato + " " + 
+                    "Lettuce: " + lettuce + " " + 
+                    "Cheese: " + cheese + " " +
+                    "Sauce: " + sauce + " " + 
+                    "Bbq Sauce: " + bbq + " " + 
+                    "Ketchup Sauce: " + ketchup);
+                Console.WriteLine("Is this correct? [Y] or [N]");
+                string confirm = Console.ReadLine();
+                // if (!string.IsNullOrWhiteSpace(confirm) && confirm == "Y") {
+                //     Console.WriteLine("How would you like to make your payment?");
+                // }
+            }
         } else {
             Console.WriteLine("Sign Up!");
         }

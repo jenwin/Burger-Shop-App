b74f7db69645db15e8823b7bebe23fc45f3b88e0
jen <nguy.jenni@gmail.com>
2022-03-27 14:35:04 -0500

    correct file names

--- BackToPaymentOptions.cs
+++ BackToPaymentOptions.cs
@@ -1,44 +1,18 @@
 using System;
-public class Orders {
-    public Orders() {
-        //taking orders
-        Console.WriteLine("Can I take your order? [Y] or [N]");
-        string customerOrder = Console.ReadLine();
-            if (!string.IsNullOrWhiteSpace(customerOrder) &&
-                customerOrder == "Y") {
-                Console.WriteLine("Would you like tomatoes? [Y] or [N]");
-                string tomato = Console.ReadLine();
-                // Console.WriteLine("Would you like lettuce? [Y] or [N]");
-                // string lettuce = Console.ReadLine();
-                // Console.WriteLine("Would you like cheese? [Y] or [N]");
-                // string cheese = Console.ReadLine();
-                // Console.WriteLine("Would you like sauce? [Y] or [N]");
-                // string sauce = Console.ReadLine();
-                // Console.WriteLine("Would you like bbq? [Y] or [N]");
-                // string bbq = Console.ReadLine();
-                // Console.WriteLine("Would you like ketchup? [Y] or [N]");
-                // string ketchup = Console.ReadLine();
-                Console.WriteLine("Please confirm your order.");
-                //prints out customer order
-                Console.WriteLine(
-                    "Tomato: " + tomato);
-                    // "Lettuce: " + lettuce + " " + 
-                    // "Cheese: " + cheese + " " +
-                    // "Sauce: " + sauce + " " + 
-                    // "Bbq Sauce: " + bbq + " " + 
-                    // "Ketchup Sauce: " + ketchup);
-                //[Y] if the customer order is correct
-                //[N] allows customer to reorder
-                Console.WriteLine("Is this correct? [Y] or [N]");
-                Payments payment = new Payments();
+public class BackToPaymentOptions {
+    public BackToPaymentOptions() {
+        Console.WriteLine("How would you like to pay?");
+        Console.WriteLine("Credit Card [A] or Debit Card [B]");
+        string payment = Console.ReadLine();
+
+        //[A] for credit card payment option
+        if (!string.IsNullOrWhiteSpace(payment) && payment == "A") {
+            CreditCard credit = new CreditCard();
             }
 
-            //end of order if no order is taken
-            do { 
-                if (!string.IsNullOrWhiteSpace(customerOrder) &&
-                customerOrder == "N") {
-                    Console.WriteLine("Goodbye.");
-                }
-            } while (false);
+        //[B] for debit card payment option
+        if (!string.IsNullOrWhiteSpace(payment) && payment == "B") {
+            DebitCard debit = new DebitCard();
+        }
     }
 }
---
+++ Orders.cs
@@ -0,0 +1,44 @@
+using System;
+public class Orders {
+    public Orders() {
+        //taking orders
+        Console.WriteLine("Can I take your order? [Y] or [N]");
+        string customerOrder = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(customerOrder) &&
+                customerOrder == "Y") {
+                Console.WriteLine("Would you like tomatoes? [Y] or [N]");
+                string tomato = Console.ReadLine();
+                // Console.WriteLine("Would you like lettuce? [Y] or [N]");
+                // string lettuce = Console.ReadLine();
+                // Console.WriteLine("Would you like cheese? [Y] or [N]");
+                // string cheese = Console.ReadLine();
+                // Console.WriteLine("Would you like sauce? [Y] or [N]");
+                // string sauce = Console.ReadLine();
+                // Console.WriteLine("Would you like bbq? [Y] or [N]");
+                // string bbq = Console.ReadLine();
+                // Console.WriteLine("Would you like ketchup? [Y] or [N]");
+                // string ketchup = Console.ReadLine();
+                Console.WriteLine("Please confirm your order.");
+                //prints out customer order
+                Console.WriteLine(
+                    "Tomato: " + tomato);
+                    // "Lettuce: " + lettuce + " " + 
+                    // "Cheese: " + cheese + " " +
+                    // "Sauce: " + sauce + " " + 
+                    // "Bbq Sauce: " + bbq + " " + 
+                    // "Ketchup Sauce: " + ketchup);
+                //[Y] if the customer order is correct
+                //[N] allows customer to reorder
+                Console.WriteLine("Is this correct? [Y] or [N]");
+                Payments payment = new Payments();
+            }
+
+            //end of order if no order is taken
+            do { 
+                if (!string.IsNullOrWhiteSpace(customerOrder) &&
+                customerOrder == "N") {
+                    Console.WriteLine("Goodbye.");
+                }
+            } while (false);
+    }
+}

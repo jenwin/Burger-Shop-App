e5a48d92e8cd438fccfbbe6518b71835f57b2309
jen <nguy.jenni@gmail.com>
2022-03-27 13:41:04 -0500

    add comments to OptionC class

--- OptionC.cs
+++ OptionC.cs
@@ -4,11 +4,15 @@ public class OptionC {
         Console.WriteLine("How would you like to pay?");
         Console.WriteLine("Credit Card [A] or Debit Card [B]");
         string payment = Console.ReadLine();
+
+        //[A] for credit card payment option
         if (!string.IsNullOrWhiteSpace(payment) && payment == "A") {
             CreditCard credit = new CreditCard();
             }
+
+        //[B] for debit card payment option
         if (!string.IsNullOrWhiteSpace(payment) && payment == "B") {
             DebitCard debit = new DebitCard();
         }
-}
+    }
 }
